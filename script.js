180fb2d4680ea18456c8d3802410738f197fbf4f
Luciano <lucianoezequiel30@gmail.com>
2024-06-30 06:31:12 -0300

    act port

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const textoCompleto = "Coming soon projects I'm working on...";
-const textoElemento = document.querySelector('.texto-infinito');
+// const textoCompleto = "Coming soon projects I'm working on...";
+// const textoElemento = document.querySelector('.texto-infinito');
 
-function escribirTexto(texto, elemento, index = 0) {
-  if (index < texto.length) {
-    elemento.textContent += texto.charAt(index);
-    index++;
-    setTimeout(() => escribirTexto(texto, elemento, index), 70); 
-  } else {
-    setTimeout(() => {
-      elemento.textContent = ''; 
-      escribirTexto(texto, elemento); 
-    }, 1000);
-  }
-}
+// function escribirTexto(texto, elemento, index = 0) {
+//   if (index < texto.length) {
+//     elemento.textContent += texto.charAt(index);
+//     index++;
+//     setTimeout(() => escribirTexto(texto, elemento, index), 70); 
+//   } else {
+//     setTimeout(() => {
+//       elemento.textContent = ''; 
+//       escribirTexto(texto, elemento); 
+//     }, 1000);
+//   }
+// }
 
-escribirTexto(textoCompleto, textoElemento);
+// escribirTexto(textoCompleto, textoElemento);
 
 const navToggle = document.querySelector(".bars-menu");
 const navList = document.querySelector(".nav-menu");
