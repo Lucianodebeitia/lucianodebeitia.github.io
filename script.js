72c41ddc3f03d2d10df7dca8e68083536e1114a2
Luciano <lucianoezequiel30@gmail.com>
2024-07-07 23:39:08 -0300

    change style web

--- script.js
+++ script.js
@@ -33,6 +33,65 @@ menuLinks.forEach(link => {
 });
 
 
+// ? 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll('.icons');
+    const containerWidth = document.querySelector('.cont-skills').offsetWidth;
+    const imageWidth = images[0].offsetWidth; // Supone que todas las imágenes tienen el mismo ancho
+    const margin = 10;
+
+    images.forEach((img, index) => {
+        img.style.left = `${index * imageWidth}px`; // Posiciona las imágenes en una fila continua
+    });
+
+    function animateImages() {
+        images.forEach((img) => {
+            const currentLeft = parseInt(img.style.left, 10);
+            const newLeft = currentLeft - 2; // Ajusta la velocidad del movimiento
+            img.style.left = `${newLeft}px`;
+
+            // Si la imagen ha salido completamente del contenedor, muévela al final
+            if (newLeft < -imageWidth) {
+                img.style.left = `${containerWidth + margin}px`;
+            }
+        });
+
+        requestAnimationFrame(animateImages);
+    }
+
+    animateImages();
+});
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const images = document.querySelectorAll('.icons');
+//     const containerWidth = document.querySelector('.cont-skills').offsetWidth;
+//     const imageWidth = images[0].offsetWidth; // Supone que todas las imágenes tienen el mismo ancho
+
+//     images.forEach((img, index) => {
+//         const delay = index * 2; // Ajusta el delay según sea necesario
+//         img.style.left = `${-imageWidth}px`; // Inicializa las imágenes fuera del contenedor
+//         img.style.animationDelay = `${delay}s`;
+//         // img.style.animationDuration = `${containerWidth / 20 + 20}s`; // Ajusta la duración de la animación
+//     });
+
+//     images.forEach(img => {
+//         img.addEventListener('animationiteration', () => {
+//             img.style.left = `${-imageWidth}px`;
+//         });
+//     });
+// });
+
+
+
+
 
 
 
